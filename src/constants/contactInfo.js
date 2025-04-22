081418c353cbef02b84784cc68cc516b0a85d616
Finaarthika <finaarthika@gmail.com>
2025-04-22 07:48:52 +0530

    fix: Update contact email address

--- src/constants/contactInfo.js
+++ src/constants/contactInfo.js
@@ -1,4 +1,4 @@
-export const EMAIL = 'info@aarthika.com';
+export const EMAIL = 'info@aarthikafinance.com';
 export const PHONE = '+916205168541'; // Raw number for tel: link
 export const PHONE_DISPLAY = '+91-6205168541'; // Formatted for display
 export const WHATSAPP_URL = 'https://wa.me/916205168541';
